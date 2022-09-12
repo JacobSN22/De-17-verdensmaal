58f93fa5cc518a9212b1101b8493f5ab1df82e3f
Jacob <jacobsimoninielsen@gmail.com>
2022-09-12 10:29:33 +0200

    Start

---
+++ assets/js/scripts.js
@@ -0,0 +1,42 @@
+const apiData = []
+
+const getData = () => {
+
+    const endPoint = "https://api.mediehuset.net/sdg/goals";
+
+    fetch(endPoint)
+    .then((response) =>{
+
+        return response.json();
+
+    })
+    .then((data) => {
+        console.log("data", data.users);
+        apiData.push(...data.items);
+
+        console.log("apiData", apiData);
+    })
+    .catch((error) => {
+
+        console.error(error);
+
+    })
+    .finally(
+        () => {apiData.map((card, i) => renderCards(card, i))
+    });
+};
+
+const renderCards = (card, i) => {
+
+const {title, byline, id, icon} = card
+console.log("card", card);
+
+document.getElementById('mother').innerHTML += `
+<p>${id + ' ' + title}</p>
+${byline}
+<img class="fill" src='data:image/svg+xml; utf8,${icon}'  alt="icon" >
+`;
+};
+
+
+getData();
